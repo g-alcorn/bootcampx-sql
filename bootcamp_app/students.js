735293ff3fffdad377c28248c8b99c4f7de4e740
g-alcorn <griffin.alcorn@gmail.com>
2019-10-20 21:25:47 +0000

    gets students by cohort name. the cohort name and number of results to show are entered by process arguments.

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -19,17 +19,3 @@ LIMIT ${process.argv[3] || 5};
     console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
   })
 }).catch(err => console.error('query error', err.stack));
-
-// pool.query(`
-// SELECT students.id AS id, students.name AS name, cohorts.name AS cohort_name
-// FROM students
-// JOIN cohorts ON cohorts.id = cohort_id
-// WHERE cohorts.name LIKE '%${process.argv[2]}%'
-// LIMIT ${process.argv[3]};
-// `)
-// .then(res => {
-//   res.rows.forEach(user => {
-//     console.log(`${user.name} has an id of ${user.id} and was in the ${user.cohort_name} cohort`);
-//   });
-// })
-// .catch(err => console.error('query error', err.stack));
